c5341599c275e0a376dfae3edc1c61cdc858effb
leif6al3 <leif6al3@gmail.com>
2026-02-13 10:31:44 +0800

    Update TextFooter message

--- src/components/TextFooter.tsx
+++ src/components/TextFooter.tsx
@@ -13,20 +13,20 @@ export default function TextFooter() {
       <h1
         className={`absolute left-10 bottom-5 transform -translate-y-1/2 text-white text-4xl lg:text-5xl font-bold leading-tight ${playfairDisplay.className}`}
       >
-        <span className="text-gray-400">Match</span> <br /> the photo pairs
+        <span className="text-gray-400">Match</span> <br /> the photo pairs c;
       </h1>
 
       {/* Right Text */}
       <h1
         className={`absolute right-10 bottom-5 transform -translate-y-1/2 text-white text-4xl lg:text-5xl font-bold leading-tight text-right ${playfairDisplay.className}`}
       >
-        to reveal <br /> <span className="text-gray-400">the surprise</span>
+        to reveal <br /> <span className="text-gray-400">the surprise hihi</span>
       </h1>
 
       {/* Copyright */}
       <p className="absolute bottom-5 right-5 text-white text-sm opacity-15 hover:opacity-50">
         © {new Date().getFullYear()}{" "}
-        <Link href="https://visibait.com">visibait.com</Link>
+        <Link href="https://iloveyoutrisha.com">iloveyoutrisha.com</Link>
       </p>
     </>
   );
